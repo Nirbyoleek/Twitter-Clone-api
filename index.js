abd0670061eb484a5abf3b834abed9af3d7aa756
Nirbyoleek Das <nirbyoleek152001@gmail.com>
2023-09-15 00:27:22 +0530

    env fixes

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 import express from "express";
-import dotenv from "dotenv";
+import { config } from "dotenv";
 import mongoose from "mongoose";
 import cookieParser from "cookie-parser";
 
 import userRoutes from "./routes/users.js";
 import authRoutes from "./routes/auths.js";
 import tweetRoutes from "./routes/tweets.js";
+config();
 
 const app = express();
-dotenv.config();
 
 const connect = () => {
 	mongoose.set("strictQuery", false);
